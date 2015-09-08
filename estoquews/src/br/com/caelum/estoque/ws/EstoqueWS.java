83cbc54f903931c3b17f1b98b1304fbf68d2961e
Fabricio Teixeira Nascimento <fabriciotn@yahoo.com.br>
2015-09-08 13:58:40 -0300

    Fazendo os exercício de WS

--- estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -2,6 +2,8 @@ package br.com.caelum.estoque.ws;
 
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 import br.com.caelum.estoque.modelo.item.Item;
@@ -12,6 +14,8 @@ public class EstoqueWS {
 
 	private ItemDao dao = new ItemDao();
 
+	@WebMethod(operationName="todosOsItens")
+	@WebResult(name="itens")
 	public List<Item> getItens() {
 		System.out.println("Chamando todosItens()");
 		return dao.todosItens();
