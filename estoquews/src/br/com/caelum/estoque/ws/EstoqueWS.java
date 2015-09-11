8848f26b1c408e5246db1f33ac3f6816cab49843
Fabricio Teixeira Nascimento <fabriciotn@yahoo.com.br>
2015-09-10 22:25:47 -0300

    Filtros

--- estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -3,12 +3,15 @@ package br.com.caelum.estoque.ws;
 import java.util.List;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
-import javax.xml.ws.ResponseWrapper;
 
+import br.com.caelum.estoque.modelo.item.Filtro;
+import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
+import br.com.caelum.estoque.modelo.item.ListaItens;
 
 @WebService
 public class EstoqueWS {
@@ -16,10 +19,12 @@ public class EstoqueWS {
 	private ItemDao dao = new ItemDao();
 
 	@WebMethod(operationName="todosOsItens")
-	@ResponseWrapper(localName="itens")
 	@WebResult(name="itens")
-	public List<Item> getItens() {
+	public ListaItens getItens(@WebParam(name="filtros") Filtros filtros) {
 		System.out.println("Chamando todosItens()");
-		return dao.todosItens();
+		
+		List<Filtro> listaFiltros = filtros.getLista();
+		List<Item> lista = dao.todosItens(listaFiltros);
+		return new ListaItens(lista);
 	}
 }
