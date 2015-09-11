c2041d8082085149d1e3aaacaee9e2418b31f02b
Fabricio Teixeira Nascimento <fabriciotn@yahoo.com.br>
2015-09-10 22:19:09 -0300

    Realizando mais customizações no WS

--- estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -5,12 +5,10 @@ import java.util.List;
 import javax.jws.WebMethod;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.ResponseWrapper;
 
-import br.com.caelum.estoque.modelo.item.Filtro;
-import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
-import br.com.caelum.estoque.modelo.item.ListaItens;
 
 @WebService
 public class EstoqueWS {
@@ -18,11 +16,10 @@ public class EstoqueWS {
 	private ItemDao dao = new ItemDao();
 
 	@WebMethod(operationName="todosOsItens")
+	@ResponseWrapper(localName="itens")
 	@WebResult(name="itens")
-	public ListaItens getItens(Filtros filtros) {
+	public List<Item> getItens() {
 		System.out.println("Chamando todosItens()");
-		List<Filtro> lista = filtros.getLista();
-		List<Item> itensResultado = dao.todosItens(lista);
-		return new ListaItens(itensResultado);
+		return dao.todosItens();
 	}
 }
